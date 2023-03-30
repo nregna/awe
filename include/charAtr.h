d29786e6a48ea23c5693659b3d738187bab1eb64
regna <45128415+nRegna@users.noreply.github.com>
2023-03-30 11:05:44 +0200

    Update charAtr.h

--- include/charAtr.h
+++ include/charAtr.h
@@ -2,6 +2,9 @@
 #define charAtr_H_INCLUDED
 
 #include"iostream"
+#include"SDL.h"
+#include"SDL_image.h"
+#include"media.h"
 
 enum eric{
     player, idfk, idfc
@@ -10,25 +13,27 @@ enum eric{
 class GenericEntityTemplate{
 	public:
 	protected:
-        int type;
+        int type=0;
+		int pastTenseOfBeingAlive=1;
+		SDL_Surface* sprite=NULL;
 };
 
 class playerChar: GenericEntityTemplate{
 	public:
-
 		playerChar(int,int,int);
-
 	private:
-
 		int stamina=20;
 		int endurance=20;
 		int sneakiness=20;
-
 };
 
 playerChar::playerChar(int prmNo1, int prmNo2, int prmNo3): stamina(prmNo1), endurance(prmNo2), sneakiness(prmNo3){
-	printf("(◡‿◡✿)");
-	type=player;
+	type=player; pastTenseOfBeingAlive=0;
+	if(surf(,sprite)){
+		printf("(◡‿◡✿)");
+	}else{
+		printf("ಠ◡ಠ");
+	};
 };
 
 #endif
