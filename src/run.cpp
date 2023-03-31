5ea06cbc8b42c59a23e735f5b0731657b1bc4a7d
regna <45128415+nregna@users.noreply.github.com>
2023-03-31 18:54:16 +0200

    Update run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -1,9 +1,12 @@
 #include"iostream"
 #include"SDL.h"
 #include"SDL_image.h"
+#include"media.h"
 #include"poll.h"
 
 int main(int argc, char* argv[]){
+    SDL_Window* wind; SDL_Surface* windSurf;
+    init(wind, windSurf);
 	for(SDL_Event event; event.type!=SDL_QUIT;){
 		while(SDL_PollEvent(&event)!=0){
 			update(event);
