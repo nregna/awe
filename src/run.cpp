2baf4e88754dda26a19b874baeb847bf3ebb9742
regna <45128415+nregna@users.noreply.github.com>
2023-03-31 16:34:57 +0200

    Update run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -1,10 +1,12 @@
+#include"iostream"
 #include"SDL.h"
 #include"SDL_image.h"
-#include"iostream"
+#include"poll.h"
 
 int main(int argc, char* argv[]){
 	for(SDL_Event event; event.type!=SDL_QUIT;){
 		while(SDL_PollEvent(&event)!=0){
+			update(event);
 		};
 	};
 };
