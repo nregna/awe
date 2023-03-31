5114aa9e962ec323422bbaf7413576062783b143
regna <45128415+nregna@users.noreply.github.com>
2023-03-31 19:48:31 +0200

    Update poll.h

--- include/poll.h
+++ include/poll.h
@@ -1,8 +1,9 @@
 #include"iostream"
 #include"SDL.h"
 #include"SDL_image.h"
+#include"media.h"
 
-bool update(SDL_Event& arg){
+bool update(SDL_Event& arg, SDL_Window*& winNo1, SDL_Surface*& surfNo1){
 	if(arg.type==SDL_KEYDOWN){
 		switch(arg.key.keysym.sym){
 			case SDLK_BACKSPACE:
@@ -11,6 +12,13 @@ bool update(SDL_Event& arg){
 			case SDLK_ESCAPE:
 				arg.type=SDL_QUIT;
 				break;
+            case SDLK_SPACE:
+                SDL_Surface* surfNo2=NULL;
+                surf(surfNo2, "44.png");
+                SDL_BlitSurface(surfNo2, NULL, surfNo1, NULL);
+                free(surfNo2);
+                SDL_UpdateWindowSurface(winNo1);
+                break;
 		};
 	};
 };
