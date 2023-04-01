3c359c6ea3da372589527443ef2968a8b3c69473
regna <45128415+nregna@users.noreply.github.com>
2023-04-01 21:58:31 +0200

    Update poll.h

--- include/poll.h
+++ include/poll.h
@@ -1,9 +1,11 @@
 #include"iostream"
 #include"SDL.h"
 #include"SDL_image.h"
-#include"media.h"
+#include"include\rendr.h"
 
-bool update(SDL_Event& arg, SDL_Window*& winNo1, SDL_Surface*& surfNo1){
+bool slide(SDL_Window*&, SDL_Surface*&);
+
+bool menuPoll(SDL_Event& arg, SDL_Window*& winNo1, SDL_Surface*& surfNo1){
 	if(arg.type==SDL_KEYDOWN){
 		switch(arg.key.keysym.sym){
 			case SDLK_BACKSPACE:
@@ -12,13 +14,20 @@ bool update(SDL_Event& arg, SDL_Window*& winNo1, SDL_Surface*& surfNo1){
 			case SDLK_ESCAPE:
 				arg.type=SDL_QUIT;
 				break;
+            case SDLK_RETURN:
+                slide(winNo1, surfNo1);
+                break;
             case SDLK_SPACE:
-                SDL_Surface* surfNo2=NULL;
-                surf(surfNo2, "44.png");
-                SDL_BlitSurface(surfNo2, NULL, surfNo1, NULL);
-                free(surfNo2);
-                SDL_UpdateWindowSurface(winNo1);
+                slide(winNo1, surfNo1);
                 break;
 		};
 	};
+};
+
+//Copy surface with image to window surface.
+bool slide(SDL_Window*& winPrm, SDL_Surface*& surfPrm){
+    Draw draw;
+    SDL_Surface* surfNo2=NULL; draw.surf(surfPrm, surfNo2, "44.png");
+    SDL_BlitSurface(surfNo2, NULL, surfPrm, NULL); SDL_UpdateWindowSurface(winPrm);
+    free(surfNo2);
 };
