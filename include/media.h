1f87b403ba4db624986df07c11514445233097a1
regna <45128415+nregna@users.noreply.github.com>
2023-04-07 22:40:33 +0200

    Update media.h

--- include/media.h
+++ include/media.h
@@ -4,16 +4,31 @@
 #include"SDL_image.h"
 #include"include\rendr.h"
 #include"include\poll.h"
-#include"string.h"
-using std::string;
 
-struct Windw{
-    Draw draw; SDL_Window* wind; SDL_Surface* windSurf;
+class Window{
+    public:
+        Draw draw; Screen screen;
+        bool init();
+        bool update(SDL_Event&);
+        bool close();
+    private:
+        SDL_Window* wind=NULL;
+        SDL_Renderer* ren=NULL;
+        SDL_Texture* tex=NULL;
+};
 
-    bool init(SDL_Window*& winNo1, SDL_Surface*& surfNo1){
-        draw.wind(winNo1, surfNo1);
-    };
-    bool update(SDL_Event& argNo1){
-        menuPoll(argNo1, wind, windSurf);
-    };
+bool Window::init(){
+    wind=SDL_CreateWindow("Ain't so", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 391, 293, SDL_WINDOW_SHOWN);
+    ren=SDL_CreateRenderer(wind, -1, SDL_RENDERER_ACCELERATED); SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
+    draw.texture(tex, ren);
+};
+
+bool Window::update(SDL_Event& arg){
+    screen.menu(arg, tex, ren);
+    SDL_UpdateWindowSurface(wind);
+};
+
+bool Window::close(){
+    desw(wind); dest(tex);
+    SDL_DestroyRenderer(ren); ren=NULL;
 };
