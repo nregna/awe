fb3219ef9252f63b84361e8619452f866096f5a6
regna <45128415+nregna@users.noreply.github.com>
2023-04-07 22:44:38 +0200

    Update rendr.h

--- include/rendr.h
+++ include/rendr.h
@@ -1,42 +1,30 @@
+#pragma once
 #include"iostream"
 #include"SDL.h"
 #include"SDL_image.h"
-#include"string.h"
-using std::string;
 
-void free(SDL_Surface*&);
-void des(SDL_Window*&);
+void dest(SDL_Texture*&);
+void desw(SDL_Window*&);
 
-struct Draw{
-    bool wind(SDL_Window*& winNo1, SDL_Surface*& surfNo1){
-        winNo1=SDL_CreateWindow("Ain't so", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 391, 293, SDL_WINDOW_SHOWN);
-        if(winNo1!=NULL){
-            surfNo1=SDL_GetWindowSurface(winNo1);
-            if(surfNo1!=NULL){
-                return true;
-            }else{
-                printf("%s\n", SDL_GetError());
-                return false;
-            };
-        }else{
-            printf("%s\n", SDL_GetError());
+class Draw{
+    public:
+        bool texture(SDL_Texture*&, SDL_Renderer*, const char*);
+};
+
+bool Draw::texture(SDL_Texture*& tex, SDL_Renderer* ren, const char* prm="44.png"){
+    if(SDL_Surface* surf=IMG_Load(prm); surf){
+        tex=SDL_CreateTextureFromSurface(ren, surf);
+        if(tex==NULL){
             return false;
         };
-    };
-    bool surf(SDL_Surface*& surfNo1, SDL_Surface*& surfNo2, string prmNo1="44.png"){
-        SDL_Surface* optim=NULL; optim=IMG_Load(prmNo1.c_str());
-        if(optim!=NULL){
-            surfNo2=SDL_ConvertSurface(optim, surfNo1->format, 0); free(optim);
-            return true;
-        };
-        return false;
+        SDL_FreeSurface(surf);
     };
 };
 
-void free(SDL_Surface*& surfNo1){
-	SDL_FreeSurface(surfNo1); surfNo1=NULL;
+void dest(SDL_Texture*& tex){
+    SDL_DestroyTexture(tex); tex=NULL;
 };
 
-void des(SDL_Window*& winNo1){
-	SDL_DestroyWindow(winNo1); winNo1=NULL;
+void desw(SDL_Window*& wind){
+	SDL_DestroyWindow(wind); wind=NULL;
 };
